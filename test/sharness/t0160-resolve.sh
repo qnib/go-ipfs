f3fe200342b6e26135191546c5404d905cf8ff5d
Matt Bell <mappum@gmail.com>
2015-09-07 14:14:50 -0700

    Consolidated 'resolve' tests
    License: MIT
    Signed-off-by: Matt Bell <mappum@gmail.com>

--- test/sharness/t0160-resolve.sh
+++ test/sharness/t0160-resolve.sh
@@ -14,13 +14,29 @@ test_expect_success "resolve: prepare files" '
 	c_hash=$(ipfs add -q -r a/b/c | tail -n1)
 '
 
-test_expect_success "resolve: prepare name" '
-	id_hash=$(ipfs id -f="<id>") &&
-	ipfs name publish "$a_hash" &&
-	printf "/ipfs/$a_hash" >expected_nameval &&
-	ipfs name resolve >actual_nameval &&
-	test_cmp expected_nameval actual_nameval
-'
+test_resolve_setup_name() {
+	ref=$1
+
+	test_expect_success "resolve: prepare name" '
+		id_hash=$(ipfs id -f="<id>") &&
+		ipfs name publish "$ref" &&
+		printf "$ref" >expected_nameval &&
+		ipfs name resolve >actual_nameval &&
+		test_cmp expected_nameval actual_nameval
+	'
+}
+
+test_resolve_setup_name_fail() {
+	ref=$1
+
+	test_expect_failure "resolve: prepare name" '
+		id_hash=$(ipfs id -f="<id>") &&
+		ipfs name publish "$ref" &&
+		printf "$ref" >expected_nameval &&
+		ipfs name resolve >actual_nameval &&
+		test_cmp expected_nameval actual_nameval
+	'
+}
 
 test_resolve() {
 	src=$1
@@ -41,10 +57,19 @@ test_resolve_cmd() {
 	test_resolve "/ipfs/$a_hash" "/ipfs/$a_hash"
 	test_resolve "/ipfs/$a_hash/b" "/ipfs/$b_hash"
 	test_resolve "/ipfs/$a_hash/b/c" "/ipfs/$c_hash"
+	test_resolve "/ipfs/$b_hash/c" "/ipfs/$c_hash"
 
+	test_resolve_setup_name "/ipfs/$a_hash"
 	test_resolve "/ipns/$id_hash" "/ipfs/$a_hash"
 	test_resolve "/ipns/$id_hash/b" "/ipfs/$b_hash"
 	test_resolve "/ipns/$id_hash/b/c" "/ipfs/$c_hash"
+
+	test_resolve_setup_name "/ipfs/$b_hash"
+	test_resolve "/ipns/$id_hash" "/ipfs/$b_hash"
+	test_resolve "/ipns/$id_hash/c" "/ipfs/$c_hash"
+
+	test_resolve_setup_name "/ipfs/$c_hash"
+	test_resolve "/ipns/$id_hash" "/ipfs/$c_hash"
 }
 
 #todo remove this once the online resolve is fixed
@@ -62,15 +87,38 @@ test_resolve_fail() {
 	'
 }
 
+
+test_resolve_success_fail() {
+	src=$1
+	dst=$2
+
+	test_expect_success "resolve succeeds: $src" '
+		ipfs resolve "$src" >actual
+	'
+
+	test_expect_failure "resolved correctly: $src -> $dst" '
+		printf "$dst" >expected &&
+		test_cmp expected actual
+	'
+}
+
 test_resolve_cmd_fail() {
+	test_resolve "/ipfs/$a_hash" "/ipfs/$a_hash"
+	test_resolve "/ipfs/$a_hash/b" "/ipfs/$b_hash"
+	test_resolve "/ipfs/$a_hash/b/c" "/ipfs/$c_hash"
+	test_resolve "/ipfs/$b_hash/c" "/ipfs/$c_hash"
 
-	test_resolve_fail "/ipfs/$a_hash" "/ipfs/$a_hash"
-	test_resolve_fail "/ipfs/$a_hash/b" "/ipfs/$b_hash"
-	test_resolve_fail "/ipfs/$a_hash/b/c" "/ipfs/$c_hash"
-
-	test_resolve_fail "/ipns/$id_hash" "/ipfs/$a_hash"
+	test_resolve_setup_name_fail "/ipfs/$a_hash"
+	test_resolve_success_fail "/ipns/$id_hash" "/ipfs/$a_hash"
 	test_resolve_fail "/ipns/$id_hash/b" "/ipfs/$b_hash"
 	test_resolve_fail "/ipns/$id_hash/b/c" "/ipfs/$c_hash"
+
+	test_resolve_setup_name_fail "/ipfs/$b_hash"
+	test_resolve_success_fail "/ipns/$id_hash" "/ipfs/$b_hash"
+	test_resolve_fail "/ipns/$id_hash/c" "/ipfs/$c_hash"
+
+	test_resolve_setup_name_fail "/ipfs/$c_hash"
+	test_resolve "/ipns/$id_hash" "/ipfs/$c_hash"
 }
 
 # should work offline
